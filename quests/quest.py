8ae49d902ee1b355e2a4c2d95a587874ea02d710
Nicholas Boni <nick@nicholasboni.com>
2024-12-23 15:05:26 -1000

    create quests

---
+++ quests/quest.py
@@ -0,0 +1,14 @@
+class Quest:
+    """A Quest is a sequence of actions that can be taken by a user to move
+    a storyline along.
+
+    Quests contain a set of related characters, decisions, and outcomes.
+    """
+    def __init__(
+            self,
+            location,
+            characters=[],
+            stages=[],
+            items=[]
+    ):
+        pass
